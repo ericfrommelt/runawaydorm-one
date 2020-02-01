7966d747da3805968ba6169dd9ade5d14b8a27fa
Eric Frommelt <ericfrommelt@gmail.com>
2020-01-31 23:29:37 -0500

    one

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styles from "./index.module.css"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
@@ -7,10 +8,14 @@ import SEO from "../components/seo"
 const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
-    <div>
-      <Img fluid={props.data.imageTwo.childImageSharp.fluid} />
-      <Img fluid={props.data.imageOne.childImageSharp.fluid} />
-      <Img fluid={props.data.imageThree.childImageSharp.fluid} />
+    <div className={styles.bg}></div>
+    <div className={styles.collage}>
+      <Img className={styles.imgtwo} fluid={props.data.imageTwo.childImageSharp.fluid} />
+      <Img className={styles.imgone} fluid={props.data.imageOne.childImageSharp.fluid} />
+      <Img className={styles.imgthree} fluid={props.data.imageThree.childImageSharp.fluid} />
+      <div className={styles.blockone}></div>
+      <div className={styles.blocktwo}></div>
+      <div className={styles.blockthree}></div>
     </div>
   </Layout>
 )
@@ -21,21 +26,21 @@ export const query = graphql`
   query {
     imageOne: file(relativePath: { eq: "DSCF3682.jpg"}) {
       childImageSharp {
-        fluid(maxWidth: 1000) {
+        fluid(maxWidth: 3000) {
           ...GatsbyImageSharpFluid
         }
       }
     }
     imageTwo: file(relativePath: { eq: "DSCF3683.jpg"}) {
       childImageSharp {
-        fluid(maxWidth: 1000) {
+        fluid(maxWidth: 3000) {
           ...GatsbyImageSharpFluid
         }
       }
     }
     imageThree: file(relativePath: { eq: "DSCF3687.jpg"}) {
       childImageSharp {
-        fluid(maxWidth: 1000) {
+        fluid(maxWidth: 3000) {
           ...GatsbyImageSharpFluid
         }
       }
