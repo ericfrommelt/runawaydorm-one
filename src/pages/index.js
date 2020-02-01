ad5aa6194743690f823d4234177ca7fb9547c0ca
Eric Frommelt <ericfrommelt@gmail.com>
2020-01-31 20:08:47 -0500

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = (props) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <Img fluid={props.data.imageTwo.childImageSharp.fluid} />
+      <Img fluid={props.data.imageOne.childImageSharp.fluid} />
+      <Img fluid={props.data.imageThree.childImageSharp.fluid} />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "DSCF3682.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageTwo: file(relativePath: { eq: "DSCF3683.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    imageThree: file(relativePath: { eq: "DSCF3687.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
